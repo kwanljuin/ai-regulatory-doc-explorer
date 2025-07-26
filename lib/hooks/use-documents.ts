4043e2ce4c17443e20b349c9e6a4099c118e61fc
kwanljuin <kwan.ljuin@gmail.com>
2025-07-27 00:16:27 +0800

    cleanup: remove unused code

--- lib/hooks/use-documents.ts
+++ lib/hooks/use-documents.ts
@@ -1,4 +1,4 @@
-import { useQuery, useInfiniteQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { secEdgarClient } from "@/lib/sec-edgar/client";
 import { DocumentFilters } from "@/types";
 
@@ -14,14 +14,5 @@ export const useDocuments = (filters: DocumentFilters) => {
       lastPage.hasMore ? allPages.length * (filters.limit || 50) : undefined,
     initialPageParam: 0,
     staleTime: 5 * 60 * 1000,
-  });
-};
-
-export const useDocumentContent = (accessionNumber: string) => {
-  return useQuery({
-    queryKey: ["document-content", accessionNumber],
-    queryFn: () => secEdgarClient.getDocumentContent(accessionNumber),
-    enabled: !!accessionNumber,
-    staleTime: 30 * 60 * 1000, // 30 minutes
   });
 };
